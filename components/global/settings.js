8957d3d33daf5f592d1fe5d548d3284bde71e395
Andrey Starkov <im@andreystarkov.ru>
2016-07-22 17:05:56 +0500

    <3

--- components/global/settings.js
+++ components/global/settings.js
@@ -2,12 +2,12 @@
 * @Author: Andrey Starkov
 * @Date:   2016-06-12 19:23:29
 * @Last Modified by:   Andrey Starkov
-* @Last Modified time: 2016-06-12 19:25:08
+* @Last Modified time: 2016-07-22 14:33:19
 */
 
 const settings = {
   videos: {
-    url: 'http://andreystarkov.dev/assets/videos/'
+    url: 'http://andreystarkov.ru/assets/videos/'
   }
 }
 
